96ee047f3264155bc8b1a81ed458aa09a5b0c1e2
piysuh arroa <piyush.arora@cybernetyx.com>
2021-02-24 00:38:53 +0530

    netlify added

--- pages/selfie.tsx
+++ pages/selfie.tsx
@@ -1,4 +1,4 @@
-import React, { LegacyRef, MutableRefObject, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { useAlert } from "react-alert";
 import Webcam from "react-webcam";
 import Header from "../components/header";
